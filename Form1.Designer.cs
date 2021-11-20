5c4bd103670941c26654759f86f8d82700326d85
borakutay <bora.keskinoglu@student.um.si>
2021-11-20 12:37:54 +0300

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Vigenere_Cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.txtEncrypted = new System.Windows.Forms.TextBox();
+            this.txtDecrypted = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(298, 112);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(152, 76);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "ofd";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(320, 42);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(100, 22);
+            this.txtKey.TabIndex = 1;
+            this.txtKey.Text = "BORA";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(298, 235);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(152, 76);
+            this.btnDecrypt.TabIndex = 2;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // txtEncrypted
+            // 
+            this.txtEncrypted.Location = new System.Drawing.Point(34, 63);
+            this.txtEncrypted.Multiline = true;
+            this.txtEncrypted.Name = "txtEncrypted";
+            this.txtEncrypted.Size = new System.Drawing.Size(173, 271);
+            this.txtEncrypted.TabIndex = 3;
+            // 
+            // txtDecrypted
+            // 
+            this.txtDecrypted.Location = new System.Drawing.Point(571, 63);
+            this.txtDecrypted.Multiline = true;
+            this.txtDecrypted.Name = "txtDecrypted";
+            this.txtDecrypted.Size = new System.Drawing.Size(173, 271);
+            this.txtDecrypted.TabIndex = 4;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(122, 372);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(502, 32);
+            this.btnOpenFile.TabIndex = 5;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // lblKey
+            // 
+            this.lblKey.AutoSize = true;
+            this.lblKey.Location = new System.Drawing.Point(353, 22);
+            this.lblKey.Name = "lblKey";
+            this.lblKey.Size = new System.Drawing.Size(32, 17);
+            this.lblKey.TabIndex = 6;
+            this.lblKey.Text = "Key";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKey);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.txtDecrypted);
+            this.Controls.Add(this.txtEncrypted);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "Form1";
+            this.Text = "Vigenere Cipher App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.TextBox txtEncrypted;
+        private System.Windows.Forms.TextBox txtDecrypted;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label lblKey;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vigenere_Cipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+     private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void Encrypt(ref StringBuilder sb, string key)
+        {
+            for (int i = 0; i < sb.Length; i++)
+            {
+                sb[i] = char.ToUpper(sb[i]);
+            }
+            key = key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (char.IsLetter(sb[i]))
+                {
+                    sb[i] = (char)(sb[i] + key[i] - 'A');
+                    if (sb[i] > 'Z') {
+                        sb[i] = (char)(sb[i] - 'Z' + 'A' - 1);
+                            }
+                    j = j + 1 == key.Length ? 0 : j + 1;
+                }
+            }
+
+        }
+        public void Decrypt(ref StringBuilder sb, string key)
+        {
+            for(int i = 0; i < sb.Length; i++)
+            {
+                sb[i] = char.ToUpper(sb[i]);
+            }
+            key = key.ToUpper();
+            int j = 0;
+            for(int i = 0; i < sb.Length; i++)
+            {
+                if (char.IsLetter(sb[i]))
+                {
+                    sb[i] = sb[i] >= key[j] ? (char)(sb[i] - key[j] + 'A') :
+                        (char)('A' + ('Z' - key[j] + sb[i] - 'A') + 1);
+                }
+                j = j + 1 == key.Length ? 0 : j + 1;
+            }
+            
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = ofd.FileName;
+                StreamReader sr = new StreamReader(File.OpenRead(filePath));
+                txtEncrypted.Text = sr.ReadToEnd();
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder(txtEncrypted.Text);
+                string key = txtKey.Text;
+
+                lblKey.Text = key;
+                Encrypt(ref sb, key);
+                txtDecrypted.Text = Convert.ToString(sb);
+            }
+            catch
+            {
+                MessageBox.Show("ERROR");
+            }
+
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StringBuilder sb = new StringBuilder(txtEncrypted.Text);
+                string key = txtKey.Text;
+
+                lblKey.Text = key;
+                Decrypt(ref sb, key);
+                txtDecrypted.Text = Convert.ToString(sb);
+            }
+            catch
+            {
+                MessageBox.Show("ERROR");
+            }
+
+        }
+
+
+    }
+}
